42e8386d2ec29f84a9954407ae43fee013a841bc
Miguel GP <miguel.gironp@gmail.com>
2024-02-23 16:21:56 +0100

    Finish Infernal chat server

--- coding_exercises/chat_server/src/main.rs
+++ coding_exercises/chat_server/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncWriteExt, BufReader, AsyncBufReadExt},
+    net::TcpListener,
+    sync::broadcast,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(10);
+    loop{
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+            let mut line: String = String::new();
+            
+            loop{
+                tokio::select!{
+                    _ = reader.read_line(&mut line) => {
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+
+                    result = rx.recv() => {
+                        let (msg, msg_addr) = result.unwrap();
+                        if addr != msg_addr {
+                          writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
