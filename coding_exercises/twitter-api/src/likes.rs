1f1b398d709b49a9a39da7f8226b8989477afddd
Miguel GP <miguel.gironp@gmail.com>
2024-03-10 19:14:33 +0100

    Fixed likes diesel and db operations

--- coding_exercises/twitter-api/src/likes.rs
+++ coding_exercises/twitter-api/src/likes.rs
@@ -1,7 +1,32 @@
 use actix_web::{HttpResponse, get, post, delete};
-use actix_web::web::Path;
-
+use actix_web::web::{Path, Data};
+use diesel::r2d2::{Pool, ConnectionManager};
+use diesel::PgConnection;
+use diesel::{ Insertable, Queryable, RunQueryDsl };
+use diesel::query_dsl::methods::{ FilterDsl, LimitDsl, OrderDsl };
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+use chrono::{NaiveDateTime, Utc};
+use super::schema::likes;
 use crate::constants::APPLICATION_JSON;
+
+#[table_name = "likes"]
+#[derive(Queryable, Insertable, Serialize, Deserialize)]
+pub struct Like {
+    id: Uuid,
+    created_at: NaiveDateTime,
+    tweet_id: Uuid,
+}
+
+impl Like {
+    fn new(tweet_id: Uuid) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            tweet_id,
+            created_at: Utc::now().naive_utc(),
+        }
+    }
+}
 
 #[get("/tweets/{id}/likes")]
 pub async fn get_likes_by_tweet(path: Path<(String, )>) -> HttpResponse {
@@ -12,11 +37,19 @@ pub async fn get_likes_by_tweet(path: Path<(String, )>) -> HttpResponse {
 }
 
 #[post("/tweets/{id}/likes")]
-pub async fn new_like(path: Path<(String, )>) -> HttpResponse {
-    let like = "ok";
+pub async fn new_like(path: Path<(String, )>, pool: Data<Pool<ConnectionManager<PgConnection>>>) -> HttpResponse {
+    use crate::schema::likes::dsl::*;
+    let t_id = &path.0;
+    let mut conn = pool.get().expect("couldn't get db connection from pool");
+
+    let like = Like::new(Uuid::parse_str(t_id).unwrap());
+    diesel::insert_into(likes)
+        .values(&like)
+        .execute(&mut conn)
+        .expect("couldn't insert like into db");
     HttpResponse::Created()
     .content_type(APPLICATION_JSON)
-    .json(like)
+    .json(&like)
 }
 
 #[delete("/tweets/{id}/likes")]
--- coding_exercises/twitter-api/src/tweets.rs
+++ coding_exercises/twitter-api/src/tweets.rs
@@ -1,19 +1,57 @@
 use actix_web::{HttpResponse, get, post, delete};
-use actix_web::web::Path;
-
+use actix_web::web::{Path, Data};
+use diesel::r2d2::{Pool, ConnectionManager};
+use diesel::PgConnection;
+use diesel::{ Insertable, Queryable, RunQueryDsl, ExpressionMethods };
+use diesel::query_dsl::methods::{ FilterDsl, LimitDsl, OrderDsl };
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+use chrono::{NaiveDateTime, Utc};
+use super::schema::tweets;
 use crate::constants::APPLICATION_JSON;
 
+#[diesel(table_name = tweets)]
+#[derive(Queryable, Insertable, Serialize, Deserialize)]
+struct Tweet {
+    id: Uuid,
+    created_at: NaiveDateTime,
+    message: String,
+}
+
+impl Tweet {
+    fn new(message: String) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            created_at: Utc::now().naive_utc(),
+            message,
+        }
+    }
+}
+
 #[get("/tweets")]
-pub async fn get_tweets() -> HttpResponse {
-    let tweets = ["tweet 1: hola", "tweet 2: chao"];
+pub async fn get_tweets(pool: Data<Pool<ConnectionManager<PgConnection>>>) -> HttpResponse {
+    use crate::schema::tweets::dsl::*;
+
+    let mut conn = pool.get().expect("couldn't get db connection from pool");
+    let result = tweets.order(created_at).limit(10).load::<Tweet>(&mut conn);
+    let response = match result {
+        Ok(tws) => tws,
+        Err(_) => vec![], // empty vec on error
+    };
+
     HttpResponse::Ok()
-    .content_type(APPLICATION_JSON)
-    .json(tweets)
+        .content_type(APPLICATION_JSON)
+        .json(response)
 }
 
 #[post("/tweets")]
-pub async fn create_tweet() -> HttpResponse {
-    let new_tweet = "new tweet";
+pub async fn create_tweet(req_body: String, pool: Data<Pool<ConnectionManager<PgConnection>>>) -> HttpResponse {
+    let new_tweet = Tweet::new(req_body);
+    let mut conn = pool.get().expect("couldn't get db connection from pool");
+    diesel::insert_into(tweets::table)
+        .values(&new_tweet)
+        .execute(&mut conn)
+        .expect("couldn't insert tweet into db");
     HttpResponse::Created()
     .content_type(APPLICATION_JSON)
     .json(new_tweet)
