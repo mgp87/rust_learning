38cd283ffe9867bb77b36230245d1c7809c4a85f
mgp-lt2ai <miguel.gironp@gmail.com>
2025-11-24 19:59:38 +0100

    Added async chat project

--- async_rust/src/main.rs
+++ async_rust/src/main.rs
@@ -1,3 +1,22 @@
+use async_std::{fs::File, io, prelude::*, task};
+
+async fn read_file(path: &str) -> io::Result<String> {
+    let mut file = File::open(path).await?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await?;
+    Ok(contents)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task = task::spawn(async {
+        let result = read_file("read.txt").await;
+        match result {
+            Ok(k) => println!("{}", k),
+            Err(e) => println!("Error reading from file: {}", e),
+        }
+    });
+
+    println!("Task started");
+    task::block_on(task);
+    println!("Stopped task");
 }
